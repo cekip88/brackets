16bb8a306c2eedd2c1acc4cd98e9c8df8c4ed625
grammlaman <grammlaman@gmail.com>
2020-10-09 16:50:47 +0600

    complete

--- src/index.js
+++ src/index.js
@@ -1,27 +1,72 @@
 module.exports = function check(str, bracketsConfig) {
-    // your solution
-    let circle = {
-        'open' : [],
-        'close' : []
-    };
 
-    for (let i = 0; i < str.length; i++){
-        if(str[i] === '(') circle.open.push(i);
-        else if (str[i] === ')') circle.close.push(i);
-        else continue;
-    }
-    let pairsArray = [];
-    while (circle.open.length){
-        let arr = [],
-            i = 0;
-        arr[0] = circle.open.splice(0,1);
-        while (circle.close.length){
-            if(circle.close[i] < circle.open[i] && circle.open.length){
-                arr[1] = circle.close.splice(i,1);
-                break
-            } else continue
+    function checkString(str) {
+        if(typeof str !== 'string') return false;
+
+        let timedStr = [];
+        for (let i = 0; i < str.length; i++){
+            let symbol = str[i];
+            if(symbol === '{' || symbol === '}' || symbol === '[' || symbol === ']' || symbol === '(' || symbol === ')' || symbol === '|'){
+                timedStr += symbol;
+                continue
+            }
+            let numberCheck = symbol * 1;
+            if(!isNaN(numberCheck)) timedStr += symbol;
         }
-        pairsArray.push(arr)
+
+        let pairs = ['{}','[]','()','12','34','56','77','88','||'];
+
+        function strPairSplit(timedStr) {
+            let checkded = false;
+            pairs.forEach(function (el) {
+                let index = timedStr.indexOf(el);
+                if(index >= 0){
+                    checkded = true;
+                    let arr = timedStr.split(el);
+                    timedStr = '';
+                    arr.forEach(function (element) {
+                        timedStr += element;
+                    })
+                }
+            });
+            if(checkded) {
+                return timedStr;
+            } else {
+                return false
+            }
+        }
+        while(timedStr){
+            if(timedStr === false){
+                break
+            }
+            timedStr = strPairSplit(timedStr);
+        }
+
+        if(timedStr === false) return false;
+        return true
     }
-    console.log(pairsArray)
+
+    function checkArray(arr){
+        let result = true;
+        arr.forEach(function (el) {
+            let str = '';
+            el.forEach(function (element) {
+                str += element;
+            });
+            let timedResult = checkString(str);
+            if(result === true) result = timedResult;
+        });
+        return result;
+    }
+
+    let stringCheck = checkString(str),
+        arrayCheck = checkArray(bracketsConfig);
+
+    if(stringCheck && arrayCheck){
+        return true
+    } else {
+        return false
+    }
+
 };
+
